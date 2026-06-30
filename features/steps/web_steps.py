02a4ceaa40310ddebcb95b60ac87859b8f70920a
Egbe Timmy <88975749+Egbetimmy@users.noreply.github.com>
2026-06-30 21:07:44 +0100

    updated test

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -104,7 +104,34 @@ def step_impl(context, element_name):
 # to get the element id of any button
 ##################################################################
 
-## UPDATE CODE HERE ##
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower().replace(' ', '_') + '-btn'
+    context.driver.find_element(By.ID, button_id).click()
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    assert(found)
+
+@then('I should see "{text_string}" in the results')
+def step_impl(context, text_string):
+    element = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.presence_of_element_located((By.ID, 'search_results'))
+    )
+    assert(text_string in element.text)
+
+@then('I should not see "{text_string}" in the results')
+def step_impl(context, text_string):
+    element = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.presence_of_element_located((By.ID, 'search_results'))
+    )
+    assert(text_string not in element.text)
 
 ##################################################################
 # This code works because of the following naming convention:
